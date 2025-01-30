fffe631ca273e70a33f37a80759b1f285f81a2b4
Fiote <brenoft14@gmail.com>
2025-01-30 12:33:14 -0300

    começando projeto de treino

---
+++ backend/app.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+dotenv.config()
+
+
+
+
+app.listen(process.env.PORT,console.log(`Servidor rodando na porta: ${process.env.PORT}`))
